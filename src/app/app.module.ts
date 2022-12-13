6ba117f758201f702be26dc381106799cf19818f
Mohammad Arif <arif_iq@yahoo.co.in>
2022-12-13 19:18:44 +0530

    Add simple directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloWorldNgIfComponent } from './directives/hello-world-ngif/hello-world-ngif.component';
 import { HelloWorldBindingsComponent } from './hello-world/hello-world-bindings.component';
 
 @NgModule({
-  declarations: [AppComponent, HelloWorldBindingsComponent],
+  declarations: [
+    AppComponent,
+    HelloWorldBindingsComponent,
+    HelloWorldNgIfComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
